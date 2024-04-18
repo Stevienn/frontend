fa6de17b7a7c96789c0178f252744e5eabbea1e9
stevienn <stevenhartanto0603@gmail.com>
2024-04-18 17:20:13 +0700

    feat: buat display product dan fungsi add to cart

---
+++ src/Components/ProductDisplay/ProductDisplayHeadset.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import './ProductDisplay.css'
+
+export const ProductDisplayHeadset = (props) => {
+    const {product} = props
+    const [jumlah, setJumlah] = useState(1);
+    const addButton = () =>{
+        setJumlah(jumlah+1);
+    }
+    const removeButton = () =>{
+        setJumlah(jumlah-1);
+        if(jumlah === 1){
+            setJumlah(1);
+        }
+    }
+    const [total, setTotal] = useState(product.price)
+    useEffect(() => {
+        setTotal(product.price * jumlah);
+    }, [jumlah]);
+    return (
+        <main className='product-display'>
+            <section className="product-display-left">
+                <img src={product.image} alt="" />
+            </section>
+            <section className="product-display-middle">
+                <article className='title'>
+                    <h1>{product.name}</h1>
+                    <h2>Rp. {product.price}</h2>
+                </article>
+                <article className="first-container">
+                    <p>Kategori : {product.category}</p>
+                    <p>Brand : {product.brand}</p>
+                </article>
+                <article className="second-container">
+                    <p>Deskripsi :</p>
+                    <p>{product.desc}</p>
+                </article>
+                <article className="third-container">
+                    <p>Driver : {product.driver}</p>
+                    <p>Weight : {product.weight}</p>
+                    <p>Warranty : {product.warranty}</p>
+                    <p>Frequency : {product.frequency}</p>
+                    <p>Impendance : {product.impendance}</p>
+                    <p>Connectivity : {product.connectivity}</p>
+                </article>
+            </section>
+            <section className="product-display-right">
+                <h1>Belanja Sekarang</h1>
+                <hr />
+                <article className='count-container'>
+                    <p>Jumlah</p>
+                    <button onClick={removeButton}>-</button>
+                    {jumlah}
+                    <button onClick={addButton}>+</button>
+                </article> 
+                <article className="total-container">
+                    <p>Total :</p>
+                    <h2>Rp. {total}</h2>
+                </article>
+                <button>ADD TO CART</button>
+            </section>
+        </main>
+  )
+}
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,16 +2,26 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import { Breadcrum } from '../Components/Breadcrum/Breadcrum';
-import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay';
+import { ProductDisplayMouse } from '../Components/ProductDisplay/ProductDisplayMouse';
+import { ProductDisplayKeyboard } from '../Components/ProductDisplay/ProductDisplayKeyboard';
+import { ProductDisplayHeadset } from '../Components/ProductDisplay/ProductDisplayHeadset';
+
+const componentMap = {
+  'Mouse': ProductDisplayMouse,
+  'Keyboard': ProductDisplayKeyboard,
+  'Headset': ProductDisplayHeadset,
+}
 
 export const Product = () => {
   const {all_product} = useContext(ShopContext);
   const {productId} = useParams(); //useParams to get the Id from parameters of the route
   const product = all_product.find((e)=> e.id === Number(productId))
+  const ComponentToRender = componentMap[product.category] || (() => <div>Page not found</div>)
   return (
     <div>
       <Breadcrum product={product} />
-      <ProductDisplay product={product} />
+      <ComponentToRender product={product} />
+      
     </div>
   )
 }
